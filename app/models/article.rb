99eacb1af0d800d77cb82235d6621329feae5440
p3rc1us <kervymaniego@gmail.com>
2024-02-06 20:41:00 +0800

    added validation for author_name

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
     validates :title, presence: true
+    validates :author_name, format: { with: /\A[a-zA-Z0-9]+\z/, message: "can only contain letters and numbers" }
+      
 end
