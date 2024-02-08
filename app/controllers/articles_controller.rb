10867d57b0e852d5b4ecf107a626ee31ca957673
p3rc1us <kervymaniego@gmail.com>
2024-02-08 21:10:00 +0800

    create done!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,27 @@
 class ArticlesController < ApplicationController 
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def show
-        @article = Article.find(params[:id])
-    end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-    def new
-        # @article = Article.new
-    end
+  def new
+    # @article = Article.new
+  end
 
-    def create
-    end
+  def create
+		@article = Article.new
+		@article.title = params[:title]
+		@article.body = params[:body]
+		@article.author_name = params[:author_name]
+
+		if @article.save
+			redirect_to articles_path
+		else
+			render :new
+		end
+  end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/articles', to: 'articles#index' #ArticlesController class ang hahanapin with index method
+  get '/articles', to: 'articles#index', as: :articles #ArticlesController class ang hahanapin with index method
 
   
   #new and create
-  get '/articles/new', to: 'articles#new'
+  get '/articles/new', to: 'articles#new', as: :new_article
   post '/articles', to: 'articles#create'
   
-  get '/articles/:id', to: 'articles#show'
+  get '/articles/:id', to: 'articles#show', as: :article
+  
 end
