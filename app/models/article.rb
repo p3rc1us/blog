d872fa19a1ba57274eb5a5bd1e793b436b935b12
p3rc1us <kervymaniego@gmail.com>
2024-02-12 20:04:03 +0800

    done edit/update, destroy/delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
     validates :title, presence: true
+    validates :body, presence: true
     validates :author_name, format: { with: /\A[a-zA-Z\s]+\z/, message: "can only contain letters" }
+    validates :co_author_name, format: { with: /\A[a-zA-Z\s]+\z/, message: "can only contain letters" }
       
 end
